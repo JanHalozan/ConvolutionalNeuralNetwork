1c6e1efd58fe147e87ed0bf4480ca0375578a7cf
JanHalozan <j.halozan@gmail.com>
2016-01-03 19:05:55 +0100

    Conv layer implementation start

--- src/convolutionlayer.cpp
+++ src/convolutionlayer.cpp
@@ -8,9 +8,10 @@
 
 #include "convolutionlayer.h"
 
-sf::ConvolutionLayer::ConvolutionLayer() : Layer()
+sf::ConvolutionLayer::ConvolutionLayer(unsigned long depth) : Layer(), stride(1), kernelSide(3), useZeroPadding(false)
 {
     this->type = kLayerTypeConvolutional;
+    this->reserveNeurons(depth);
 }
 
 void sf::ConvolutionLayer::calculateOutput()
@@ -21,4 +22,43 @@ void sf::ConvolutionLayer::calculateOutput()
 void sf::ConvolutionLayer::backprop(sf::Layer *previousLayer, sf::Layer *nextLayer, sf::LayerBackpropInfo *info)
 {
     
+}
+
+double sf::ConvolutionLayer::convolve(double *input, double *kernel)
+{
+    double result = 0.0;
+    
+    
+    
+    return result;
+}
+
+void sf::ConvolutionLayer::setStride(unsigned short stride)
+{
+    this->stride = stride;
+}
+
+unsigned short sf::ConvolutionLayer::getStride() const
+{
+    return this->stride;
+}
+
+void sf::ConvolutionLayer::setKernelSideSize(unsigned short size)
+{
+    this->kernelSide = size;
+}
+
+unsigned short sf::ConvolutionLayer::getKernelSideSize() const
+{
+    return this->kernelSide;
+}
+
+void sf::ConvolutionLayer::setUseZeroPadding(bool use)
+{
+    this->useZeroPadding = use;
+}
+
+bool sf::ConvolutionLayer::getUseZeroPadding() const
+{
+    return this->useZeroPadding;
 }
--- src/convolutionlayer.h
+++ src/convolutionlayer.h
@@ -19,6 +19,12 @@ namespace sf
 class sf::ConvolutionLayer : public sf::Layer
 {
 private:
+    ConvolutionLayer(unsigned long depth);
+    
+    unsigned short stride;
+    unsigned short kernelSide;
+    bool useZeroPadding;
+    
     double convolve(double *input, double *kernel);
     
 public:
@@ -26,6 +32,15 @@ public:
     
     void calculateOutput() override;
     void backprop(sf::Layer *previousLayer, sf::Layer *nextLayer, sf::LayerBackpropInfo *info) override;
+    
+    void setStride(unsigned short stride);
+    unsigned short getStride() const;
+    
+    void setKernelSideSize(unsigned short size);
+    unsigned short getKernelSideSize() const;
+    
+    void setUseZeroPadding(bool use);
+    bool getUseZeroPadding() const;
 };
 
 #endif /* convolutionlayer_h */
